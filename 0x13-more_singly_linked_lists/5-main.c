6261abc776b5eb28ba18317532aad4816b9873a0
karimgit01 <karimgit728@gmail.com>
2024-02-23 13:54:29 -0800

    update

---
+++ 0x13-more_singly_linked_lists/5-main.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    listint_t *head;
+
+    head = NULL;
+    add_nodeint_end(&head, 0);
+    add_nodeint_end(&head, 1);
+    add_nodeint_end(&head, 2);
+    add_nodeint_end(&head, 3);
+
